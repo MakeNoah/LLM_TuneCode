7605dd3ea4cd95226adc8480d505838f34c39aa8
Makenoah <tubasawing@gmail.com>
2024-12-07 23:31:57 +0900

    [refactor] pathを変更

--- 00_学習用/learning_module/save_and_upload.py
+++ 00_学習用/learning_module/save_and_upload.py
@@ -1,8 +1,11 @@
+import os
+
 def save_and_upload(model, tokenizer, config):
     """
     トレーニング済みモデルをHugging Face Hubにアップロードする。
     """
-    model.save_pretrained(config["model"]["new_model_id"])
-    tokenizer.save_pretrained(config["model"]["new_model_id"])
+    RESULT_PATH =os.path.join(config["model"]["new_model_path"],config["model"]["new_model_id"])
+    model.save_pretrained(RESULT_PATH)
+    tokenizer.save_pretrained(RESULT_PATH)
     model.push_to_hub(config["model"]["new_model_id"], use_auth_token=config["huggingface"]["hf_token"], private=config["huggingface"]["private"])
     tokenizer.push_to_hub(config["model"]["new_model_id"], use_auth_token=config["huggingface"]["hf_token"], private=config["huggingface"]["private"])
